c1cf86268a18fa01befcdae1731396bde963cc7e
Roque <164688618+PdRoque@users.noreply.github.com>
2025-03-11 00:21:35 -0300

    Update ex-02.py

--- ex-02.py
+++ ex-02.py
@@ -1,3 +1,4 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
 print("Pedro Roque")
 print("Rio de Janeiro")
 print("Duque de Caxias")
