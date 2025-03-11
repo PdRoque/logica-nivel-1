59e27f27d48e72c249fc635e699965b2df43a176
Roque <164688618+PdRoque@users.noreply.github.com>
2025-03-11 00:17:31 -0300

    Create ex-07.py

---
+++ ex-07.py
@@ -0,0 +1,12 @@
+#7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela
+print("          X")       
+print("         XXX")
+print("        XXXXX")
+print("       XXXXXXX")
+print("      XXXXXXXXX")
+print("     XXXXXXXXXXX")
+print("    XXXXXXXXXXXXX")
+print("   XXXXXXXXXXXXXXX")
+print("         XX")
+print("         XX")
+print("        XXXX")
