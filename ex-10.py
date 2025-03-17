a152a5cdd9b9c5c4daedeb6856b0b567d8eb33a6
Roque <164688618+PdRoque@users.noreply.github.com>
2025-03-17 19:52:33 -0300

    Create ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,6 @@
+#escreva um software que informe a média aritimética de um aluno que cursou 4 bimestres
+bimestre1 = int(input("qual é a nota do primeiro bimestre?"))
+bimestre2 = int(input("qual é a nota do segundo bimestre?"))
+bimestre3 = int(input("qual é a nota do terceiro bimestre?"))
+bimestre4 = int(input("qual é a nota do quarto bimestre?"))
+print((bimestre1 + bimestre2 + bimestre3 + bimestre4) / 4)
