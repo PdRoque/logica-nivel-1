a2df3b278a93f04224a97ee440a79dec55bc20d4
Roque <164688618+PdRoque@users.noreply.github.com>
2025-03-17 21:44:21 -0300

    Create ex-15.py

---
+++ ex-15.py
@@ -0,0 +1,6 @@
+#Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).
+valor = int(input("Qual é o resultado da sua conta?"))
+if (valor > -1):
+    print("positivo")
+else:
+    print("negativo")
