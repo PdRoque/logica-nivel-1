dfc2e7624c39f987a8df836a9fc4bf92f7d47ac5
Roque <164688618+PdRoque@users.noreply.github.com>
2025-03-19 20:42:58 -0300

    Create ex-11.py

---
+++ ex-11.py
@@ -0,0 +1,3 @@
+#11 - Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+numero = int(input("Digite um número para ver seu antecessor."))
+print(numero-1)
