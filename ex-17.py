fd91d71a2133a599a1b06a7b6a383a3388744d71
Roque <164688618+PdRoque@users.noreply.github.com>
2025-03-24 19:47:08 -0300

    Update ex-17.py

--- ex-17.py
+++ ex-17.py
@@ -3,15 +3,7 @@ print("Digite três números distintos.")
 valor1 = int(input("Digite o primeiro valor:"))
 valor2 = int(input("Digite o segundo valor:"))
 valor3 = int(input("Digite o terceiro valor:"))
-if(valor1>valor2):
-    print(valor1, "é maior que", valor2)
+if((valor1>valor2)>valor3):
+    print((valor>valor2))
 else:
-    print(valor2, "é maior que", valor1)
-    if(valor1>valor3):
-        print(valor1, "é maior que", valor3)
-    else:
-        print(valor3, "é maior que", valor1)
-        if(valor2>valor3):
-            print(valor2, "é maior que", valor3)
-        else:
-            print(valor3, "é maior que", valor2) 
+    print(valor3)
