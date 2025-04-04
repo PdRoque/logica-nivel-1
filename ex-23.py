866dd9c4f5c0d5a97e1d04f0c3328a6aca46ca3f
Roque <164688618+PdRoque@users.noreply.github.com>
2025-04-04 17:43:33 -0300

    Create ex-23.py

---
+++ ex-23.py
@@ -0,0 +1,13 @@
+#23) Contagem regressiva de 10 a 1
+for x in range(10,0, -1):
+    print(x)
+print("Fogo! 🚀")
+#porque para por números de 1 a 10 em ordem descrecente tem que organizar como (10, 0, -1) python?
+#A função range() tem 3 partes:
+#range(início, fim, passo)
+
+#início → número onde a contagem começa.
+
+#fim → número onde a contagem termina (não é incluído!).
+
+#passo → de quanto em quanto os números vão mudar.
