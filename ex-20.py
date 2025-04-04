3586e3345ec1c450106afde8c14c8c0e8586be81
Roque <164688618+PdRoque@users.noreply.github.com>
2025-04-04 17:17:32 -0300

    Update ex-20.py

--- ex-20.py
+++ ex-20.py
@@ -1,17 +1,15 @@
-# 20) Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente. 
-print("Digite os números de 1 a 10(podendo ser fora de ordem) pressionando ENTER após cada número digitado.")
-valor1 = int(input())
-valor2 = int(input())
-valor3 = int(input())
-valor4 = int(input())
-valor5 = int(input())
-valor6 = int(input())
-valor7 = int(input())
-valor8 = int(input())
-valor9 = int(input())
-valor10 = int(input())
+# 20) Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente.
 
-valores = [valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, valor9, valor10]
+for x in range(1,11):
+    print(x)
+#Esse é um laço de repetição (loop for).
 
-valores.sort()
-print(valores)
+#A função range(1, 11) cria uma sequência de números de 1 até 10.
+
+#O número 1 é incluído, e o número 11 não é incluído (o range vai até um antes do segundo valor).
+
+#Então o Python entende isso como: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10.
+
+#A variável "x" assume cada valor dessa sequência, um por um, a cada repetição.
+
+# print(x)
