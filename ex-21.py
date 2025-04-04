08b04df31c42bb03417936675e383d61974f62eb
Roque <164688618+PdRoque@users.noreply.github.com>
2025-04-04 17:26:17 -0300

    Create ex-21.py

---
+++ ex-21.py
@@ -0,0 +1,13 @@
+#21 ) Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente.
+
+for x in range (10,0,-1):
+    print(x)
+#Esse é um laço de repetição (for) que usa a função range().
+
+#A função range(início, fim, passo) gera uma sequência de números de acordo com os 3 parâmetros:
+
+#início = 10 → Começa do número 10.
+
+#fim = 0 → Vai até 1, porque o valor final (0) não é incluído.
+
+#passo = -1 → O número diminui de 1 em 1 (contagem decrescente).
