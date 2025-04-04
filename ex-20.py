3af598e7b773b0d344caee14e37092007ec66b67
Roque <164688618+PdRoque@users.noreply.github.com>
2025-04-04 00:11:46 -0300

    Create ex-20.py

---
+++ ex-20.py
@@ -0,0 +1,17 @@
+# 20) Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente. 
+print("Digite os números de 1 a 10(podendo ser fora de ordem) pressionando ENTER após cada número digitado.")
+valor1 = int(input())
+valor2 = int(input())
+valor3 = int(input())
+valor4 = int(input())
+valor5 = int(input())
+valor6 = int(input())
+valor7 = int(input())
+valor8 = int(input())
+valor9 = int(input())
+valor10 = int(input())
+
+valores = [valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, valor9, valor10]
+
+valores.sort()
+print(valores)
